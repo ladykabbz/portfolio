5e2436519bf3f4edc605570e462380f2c60b9a3e
ladykabbz <kabiinicole@gmail.com>
2023-02-01 22:39:25 +0300

    homepage working

---
+++ src/script.js
@@ -0,0 +1,72 @@
+anime
+  .timeline({ loop: true })
+  .add({
+    targets: ".ml5 .line",
+    opacity: [0.5, 1],
+    scaleX: [0, 1],
+    easing: "easeInOutExpo",
+    duration: 700,
+  })
+  .add({
+    targets: ".ml5 .line",
+    duration: 600,
+    easing: "easeOutExpo",
+    translateY: (el, i) => -0.625 + 0.625 * 2 * i + "em",
+  })
+  .add({
+    targets: ".ml5 .ampersand",
+    opacity: [0, 1],
+    scaleY: [0.5, 1],
+    easing: "easeOutExpo",
+    duration: 600,
+    offset: "-=600",
+  })
+  .add({
+    targets: ".ml5 .letters-left",
+    opacity: [0, 1],
+    translateX: ["0.5em", 0],
+    easing: "easeOutExpo",
+    duration: 600,
+    offset: "-=300",
+  })
+  .add({
+    targets: ".ml5 .letters-right",
+    opacity: [0, 1],
+    translateX: ["-0.5em", 0],
+    easing: "easeOutExpo",
+    duration: 600,
+    offset: "-=600",
+  })
+  .add({
+    targets: ".ml5",
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000,
+  });
+// Wrap every letter in a span
+var textWrapper = document.querySelector(".ml7 .letters");
+textWrapper.innerHTML = textWrapper.textContent.replace(
+  /\S/g,
+  "<span class='letter'>$&</span>"
+);
+
+anime
+  .timeline({ loop: true })
+  .add({
+    targets: ".ml7 .letter",
+    translateY: ["1.1em", 0],
+    translateX: ["0.55em", 0],
+    translateZ: 0,
+    rotateZ: [180, 0],
+    duration: 750,
+    easing: "easeOutExpo",
+    delay: (el, i) => 50 * i,
+  })
+  .add({
+    targets: ".ml7",
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000,
+  });
